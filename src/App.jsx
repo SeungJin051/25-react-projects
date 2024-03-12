3f810d0bf34a9170d9b26190f048af19fe268d55
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-12 21:21:29 +0900

    이미지 슬라이더

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Accordian from "./components/accordian/index";
 import RandomColor from "./components/random-color/index";
 import StarRating from "./components/star-raring/index";
+import ImageSlider from "./components/image-slider/index";
 
 function App() {
   return (
     <>
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating numOfstars={10} />
+      {/* <StarRating numOfstars={10} /> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
